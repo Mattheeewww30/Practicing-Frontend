0ff773cca5e9602a3efc8276fb5470dec5c29adb
Mattheeewww30 <mateo.arcila.1985@gmail.com>
2022-10-01 20:44:33 -0500

    Adaptación del menú para dispositivos móviles

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const popMenuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+const hamburguerIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 
 popMenuEmail.addEventListener("click", popDesktopMenu);
+hamburguerIcon.addEventListener("click", popMobileMenu);
 
 
 function popDesktopMenu (){
     desktopMenu.classList.toggle("inactive");
+}
+
+
+function popMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
 
