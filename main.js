f6149b07b4f9fb7d9c8c2b492f5d14c8096424dc
Mattheeewww30 <mateo.arcila.1985@gmail.com>
2022-10-05 17:06:31 -0500

    Implementación del carrito de compras y mejora en fallas al momento de abrir un menú mientras otro está abierto

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
-const popMenuEmail = document.querySelector(".navbar-email");
+// Email
+const emailIcon = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu")
+// In case of using mobile
 const hamburguerIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+// For shopping 
+const kartIcon = document.querySelector(".navbar-shopping-cart")
+const kartMenu = document.querySelector(".product-detail")
 
 
-popMenuEmail.addEventListener("click", popDesktopMenu);
+
+emailIcon.addEventListener("click", popDesktopMenu);
 hamburguerIcon.addEventListener("click", popMobileMenu);
+kartIcon.addEventListener("click", popKarteMenu);
 
 
 function popDesktopMenu (){
+    const isKartMenuClose = kartMenu.classList.contains("inactive")
+
+    if (!isKartMenuClose){
+        kartMenu.classList.add("inactive")
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
 
 function popMobileMenu(){
+    const isKartMenuClose = kartMenu.classList.contains("inactive")
+
+    if (!isKartMenuClose){
+        kartMenu.classList.add("inactive")
+    }
     mobileMenu.classList.toggle("inactive");
+}
+
+
+function popKarteMenu(){
+    const isMobileMenuClose = mobileMenu.classList.contains("inactive")
+
+    if (!isMobileMenuClose){
+        mobileMenu.classList.add("inactive")
+    }
+    kartMenu.classList.toggle("inactive");
 }
 
