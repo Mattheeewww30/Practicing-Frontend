18fa325217a38905ec65a9e7d77cea1d1cdee9d2
Mattheeewww30 <mateo.arcila.1985@gmail.com>
2022-10-09 08:08:24 -0500

    Se añade lista de productos con maquetación en JS

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ const mobileMenu = document.querySelector(".mobile-menu")
 // For shopping 
 const kartIcon = document.querySelector(".navbar-shopping-cart")
 const kartMenu = document.querySelector(".product-detail")
+// Products
+const cardsContainer = document.querySelector(".cards-container")
 
 
-
+// Render Menu
 emailIcon.addEventListener("click", popDesktopMenu);
 hamburguerIcon.addEventListener("click", popMobileMenu);
 kartIcon.addEventListener("click", popKarteMenu);
-
 
 function popDesktopMenu (){
     const isKartMenuClose = kartMenu.classList.contains("inactive")
@@ -24,7 +25,6 @@ function popDesktopMenu (){
     desktopMenu.classList.toggle("inactive");
 }
 
-
 function popMobileMenu(){
     const isKartMenuClose = kartMenu.classList.contains("inactive")
 
@@ -34,7 +34,6 @@ function popMobileMenu(){
     mobileMenu.classList.toggle("inactive");
 }
 
-
 function popKarteMenu(){
     const isMobileMenuClose = mobileMenu.classList.contains("inactive")
 
@@ -43,4 +42,58 @@ function popKarteMenu(){
     }
     kartMenu.classList.toggle("inactive");
 }
+
+
+// Render Products
+const productList = []
+productList.push({
+    name: "bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+productList.push({
+    name: "TV",
+    price: 200,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+productList.push({
+    name: "PC",
+    price: 300,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+function renderProducts(array){
+    for (product of array){
+        const productCard = document.createElement("div")
+        productCard.classList.add("product-card")
+    
+        const productImg = document.createElement("img")
+        productImg.setAttribute("src", product.image)
+    
+        const productInfo = document.createElement("div")
+        productInfo.classList.add("product-info")
+    
+        const productInfoDiv = document.createElement("div")
+        const productPrice = document.createElement("p")
+        productPrice.innerText = "$" + product.price
+        const productName = document.createElement("p")
+        productName.innerText = product.name
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure = document.createElement("figure")
+        const productImgCart = document.createElement("img")
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+renderProducts(productList)
 
